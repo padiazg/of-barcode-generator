41dd485706247aae8e944552e893e1435e258bd7
Patricio Diaz <padiazg@gmail.com>
2018-09-12 10:38:00 -0400

    Changed parameter format to application/x-www-form-urlencoded. Allows passing parameters in querystring or body
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

--- barcode-generator/handler.js
+++ barcode-generator/handler.js
@@ -1,34 +1,49 @@
 "use strict";
+const querystring = require("querystring");
 const bwipjs = require("bwip-js");
 
 module.exports = (context, callback) => {
-    // always receive params as a json object
-    let options;
-    try {
-        options = JSON.parse(context);
-    } catch (e) {
-        callback(e, undefined);
+    const { Http_Method, Http_Query, Http_Content_Type } = process.env;
+
+    // check method
+    if (Http_Method != "GET" && Http_Method != "POST") {
+        callback("Method not allowed", null);
+        return;
+    } // (Http_Method != "GET" && Http_Method != "POST")  ...
+
+    // check if parameters are present
+    if ((Http_Method =="GET" && !Http_Query) || (Http_Method =="POST" && !Http_Query && !context)) {
+        callback("Missing parameters", null);
         return;
     }
 
-    if (!options.bcid || !options.text) {
+    // validate parameters passed in body
+    if (Http_Method =="POST" && !Http_Query && context && Http_Content_Type != "application/x-www-form-urlencoded") {
+        callback("Content_Type not especified/allowed", null);
+        return;
+    }
+
+    const payload = Http_Query ? Http_Query : context;
+    let parameters = querystring.parse(payload);
+
+    if (!optioparametersns.bcid || !parameters.text) {
         callback('bdid & text are required', undefined);
         return;
-    } // if (!options.bcid || !options.text) ...
+    } // if (!parameters.bcid || !parameters.text) ...
 
     // encode to base64
-    const base64 = options.base64 ? options.base64 : false;
-    delete options.base64;
+    const base64 = parameters.base64 ? parameters.base64 : false;
+    delete parameters.base64;
 
     // set some defaults
-    options.bcid = options.bcid.toLowerCase();
-    options.backgroundcolor = options.backgroundcolor ? options.backgroundcolor : 'FFFFFF';
-    options.padding = options.padding ? options.padding : 1;
-    options.guardwhitespace = true;
-    options.paddingwidth = options.paddingwidth != undefined ? options.paddingwidth : 1;
-    options.paddingheight = options.paddingheight != undefined ? options.paddingheight : 1;
+    parameters.bcid = parameters.bcid.toLowerCase();
+    parameters.backgroundcolor = parameters.backgroundcolor ? parameters.backgroundcolor : 'FFFFFF';
+    parameters.padding = parameters.padding ? parameters.padding : 1;
+    parameters.guardwhitespace = true;
+    parameters.paddingwidth = parameters.paddingwidth != undefined ? parameters.paddingwidth : 1;
+    parameters.paddingheight = parameters.paddingheight != undefined ? parameters.paddingheight : 1;
 
-    bwipjs.toBuffer(options, (error, png) => {
+    bwipjs.toBuffer(parameters, (error, png) => {
         if (error) {
             callback(error, undefined);
         } else {
